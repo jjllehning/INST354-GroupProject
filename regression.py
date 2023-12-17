0d84e0bc43bfdad3f39f1e839f9ac73425e5fa9f
jjllehning <jjllehning@gmail.com>
2023-12-17 17:20:39 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -12,5 +12,20 @@ import seaborn as sns
 
 comp_df = pd.read_csv("complaint.csv")
 
-print(comp_df.head())
+X = comp_df[['Age', 'Gpa', 'Year', 'Count']]
+y = comp_df['Genre']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+LogReg = LogisticRegression()
+
+
+scaler = preprocessing.StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+LogReg.fit(X_train,y_train)
+
+
+y_pred=LogReg.predict(X_test)
 
