f28b7bce576ab6458f1656988bd26084641ddfe0
BogdanSenic <129547547+BogdanSenic@users.noreply.github.com>
2023-12-17 19:07:05 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -15,7 +15,6 @@ y = comp_df['Genre']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-# Use multinomial logistic regression
 LogReg = LogisticRegression(multi_class='multinomial', solver='newton-cg')
 
 scaler = preprocessing.StandardScaler()
