8096e8827c6f0334cbf67bc841df0cead09a10a3
BogdanSenic <129547547+BogdanSenic@users.noreply.github.com>
2023-12-17 19:01:46 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -2,30 +2,33 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
-from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn import preprocessing
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-comp_df = pd.read_csv("Datasetprojpowerbi.csv")
+comp_df = pd.read_csv("complaint.csv")
 
 X = comp_df[['Age', 'Gpa', 'Year', 'Count']]
 y = comp_df['Genre']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-LogReg = LogisticRegression()
-
+# Use multinomial logistic regression
+LogReg = LogisticRegression(multi_class='multinomial', solver='newton-cg')
 
 scaler = preprocessing.StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-LogReg.fit(X_train,y_train)
+LogReg.fit(X_train, y_train)
 
+y_pred = LogReg.predict(X_test)
 
-y_pred=LogReg.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average='weighted'))
+print("Recall:", recall_score(y_test, y_pred, average='weighted'))
 
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
