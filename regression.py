84b04d18ce9a5bbd4410ec9a90cf53bb0694187e
jjllehning <jjllehning@gmail.com>
2023-12-17 21:39:29 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -10,6 +10,8 @@ import seaborn as sns
 
 comp_df = pd.read_csv("complaint.csv")
 
+#Plot for GPA
+
 #Cut continuous, bound data into parts
 bins = [0, 2.5, 3.5, 4.5]
 labels = [1, 2, 3]
@@ -45,9 +47,47 @@ print("Confusion Matrix:\n", conf_matrix)
 
 fig, ax = plt.subplots()
 sns.heatmap(pd.DataFrame(conf_matrix), annot=True, cmap="YlGnBu" ,fmt='g', xticklabels=genres, yticklabels=genres)
-plt.title("Confusion Matrix")
+plt.title("Confusion Matrix- GPA")
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 ax.xaxis.set_label_position("top")
 plt.tight_layout()
 plt.show()
+
+
+
+#Plot for Age
+X2 = comp_df[['Age']]
+y2 = comp_df['Genre']
+
+X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.25, random_state=0)
+
+LogReg2 = LogisticRegression()
+
+scaler2 = preprocessing.StandardScaler()
+X_train2 = scaler2.fit_transform(X2_train)
+X_test2 = scaler2.transform(X2_test)
+
+LogReg2.fit(X2_train, y2_train)
+
+y2_pred = LogReg2.predict(X2_test)
+
+print("Accuracy:", accuracy_score(y2_test, y2_pred))
+print("Precision:", precision_score(y2_test, y2_pred, average='weighted'))
+print("Recall:", recall_score(y2_test, y2_pred, average='weighted'))
+
+print("Classification Report:\n", classification_report(y2_test, y2_pred))
+
+conf_matrix2 = confusion_matrix(y2_test, y2_pred)
+genres2 = comp_df['Genre'].unique()
+print("Confusion Matrix:\n", conf_matrix2)
+
+fig, ax = plt.subplots()
+sns.heatmap(pd.DataFrame(conf_matrix2), annot=True, cmap="YlGnBu" ,fmt='g', xticklabels=genres, yticklabels=genres)
+plt.title("Confusion Matrix- Age")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.show()
+
